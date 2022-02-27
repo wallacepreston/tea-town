9f63207d2e2524ed3b044a41e6f47dcaf4bc2dd0
Preston Wallace <wallace.preston@gmail.com>
2022-02-26 23:52:14 -0800

    fix(db name): Change to tea

--- db/client.js
+++ db/client.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
 // if the specified database exists, connect to it else create the database
-const db = new sqlite3.Database('./restaurants.sqlite');
+const db = new sqlite3.Database('./tea.sqlite');
 
 module.exports = db;
